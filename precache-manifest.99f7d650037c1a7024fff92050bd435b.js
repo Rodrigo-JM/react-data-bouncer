08485580c5a1cbc94a766e944b94fd8a52fb22f3
rodrigojm <rodrigomallmann1@gmail.com>
2020-05-13 19:10:52 -0400

    Updates

---
+++ precache-manifest.99f7d650037c1a7024fff92050bd435b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e819001357d4d66ba930a77b44548756",
+    "url": "/react-data-bouncer/index.html"
+  },
+  {
+    "revision": "9ad387a2b6fc2b7bee17",
+    "url": "/react-data-bouncer/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b5f4f1a4839743781732",
+    "url": "/react-data-bouncer/static/js/2.244389c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-data-bouncer/static/js/2.244389c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ad387a2b6fc2b7bee17",
+    "url": "/react-data-bouncer/static/js/main.0e5c8fbb.chunk.js"
+  },
+  {
+    "revision": "c527d377abfe59a42514",
+    "url": "/react-data-bouncer/static/js/runtime-main.8cd2f20c.js"
+  }
+]);
